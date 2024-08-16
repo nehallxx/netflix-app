6f19bef01e2f1a4bfece0f0e48f6af3694034815
nehallxx <132005828+nehallxx@users.noreply.github.com>
2024-08-17 02:37:05 +0300

    Update Home.jsx

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,1 +1,19 @@
+import "./home.scss"
+import React from 'react'
+import Navbar from "../components/navbar/Navbar"
+import vrImage from '../assets/virtual-reality.jpg';
+import Featured from "../components/featured/Featured";
+import List from "../components/list/List";
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <Featured/>
+      <List/>
+    </div>
+  )
+}
+
+export default Home
 
